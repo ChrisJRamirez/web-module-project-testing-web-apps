9e1602f5d31aba2f3fbd021d0159f27b81a6e55f
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-10-07 19:50:27 -0400

    working through tests

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -2,3 +2,11 @@
 1. What are some possible tests for this application?
 * The component correctly renders.
 * ...
+
+Can you fill out each input field for the contact form?
+
+Will there be errors if certain inputs are not long enough / the right type of input e email?
+
+Can you submit the form?
+
+Can you submit the form if there are any errors in your inputs?
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,19 +5,46 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
+  render(<ContactForm/>)
     
 });
 
 test('renders the contact form header', ()=> {
+  render(<ContactForm/>);
+
+  const headerElement = screen.queryByText(/Contact Form/i);
+  expect(headerElement).toBeInTheDocument();
+  expect(headerElement).toBeTruthy();
+  expect(headerElement).toHaveTextContent(/Contact Form/i)
     
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+  render(<ContactForm/>);
+  const firstNameField = screen.getByLabelText(/First Name*/i)
+
+  userEvent.type(firstNameField, "123");
+
+  // awaiting something that returns a promise when you use await
+  // use findAll when looking for a promise
+  const errorMessages = await screen.findAllByTestId("error");
+  // 1 error message exists = length of 1
+  expect(errorMessages).toHaveLength(1);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+  render(<ContactForm/>);
+
+  const submitButton = screen.getByRole("button");
+  userEvent.click(submitButton);
+
+  // changes on screen have happened after submit clicked, cuz we waited for them to happen
+
+  await waitFor(()=> {
+    const errorMessages = screen.queryAllByTestId("error");
+    expect(errorMessages).toHaveLength(3);
+
+  })
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
